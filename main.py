7eaf903f7a70bad7a97bbd4710ece7289b068841
Pawlo83 <94640915+Pawlo83@users.noreply.github.com>
2024-12-10 13:53:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,8 @@ def show_question():
                     question += fact[i].capitalize()
                     questionShow += fact[i] + ' '
     #                                                                                           podpis o ilości odpowiedzi mniejszy i pod ql
-    qs_label.config(text=questionShow+' (NUMBER OF POSSIBLE ANSWERS: '+str(numberOfAnswers)+')')
+    qs_label.config(text=questionShow)
+    noa_label.config(text='(NUMBER OF POSSIBLE ANSWERS: ' + str(numberOfAnswers) + ')')
 
     choices=[]
     for fact in env.facts():
@@ -69,14 +70,14 @@ def show_question():
         choice_btns_number[i] = 0
         if j < len(choices):
             choice_btns[i].config(style='Standard.TButton')
-            choice_btns[i].pack()
+            choice_btns[i].grid(row=i + 2, column=1, columnspan=3, pady=5)
             if choices[j][-1] == ":":
                 choice_btns[i].config(text=choices[j] + ' ' + choices[j + 1], state="normal")
                 j+=1
             else:
                 choice_btns[i].config(text=choices[j], state="normal")
         else:
-            choice_btns[i].pack_forget()
+            choice_btns[i].grid_forget()
             #choice_btns[i].config(style='Invis.TButton')
         i+=1
         j+=1
@@ -138,47 +139,65 @@ def next_question():
 # Create the main window
 root = tk.Tk()
 root.title("Sci-fi and Fantasy books decision flowchart")
-root.geometry("600x600")
+root.geometry("1200x500")
+root.grid_columnconfigure(0, weight=1, uniform="fred")
+root.grid_columnconfigure(1, weight=1, uniform="fred")
+root.grid_columnconfigure(2, weight=1, uniform="fred")
+root.grid_columnconfigure(3, weight=1, uniform="fred")
+root.grid_columnconfigure(4, weight=1, uniform="fred")
+
 style = Style(theme="flatly")
 style.map('TButton', background=[('active','red')])
 
 # Configure the font size for the question and choice buttons
-style.configure("TLabel", font=("Helvetica", 20))
-style.configure("TButton", font=("Helvetica", 16))
+style.configure("TLabel", font=("Consolas", 20, 'bold'), background="light grey")
 
-style.configure('Standard.TButton',
-                foreground='white',
-                background='blue',
-                font=('Helvetica', 12, 'bold'))
+style.configure(
+    'Standard.TButton',
+    foreground='white',
+    background='teal',
+    font=('Consolas', 12)
+)
 
-# Style for Button 2
-style.configure('Clicked.TButton',
-                foreground='black',
-                background='green',
-                font=('Arial', 10, 'italic'))
+style.configure(
+    'Clicked.TButton',
+    foreground='white',
+    background='dark blue',
+    font=('Consolas', 12)
+)
 
-style.configure('Invis.TButton',
-                background=[("disabled", root.cget('bg'))],
-                foreground=[("disabled", root.cget('bg'))],
-                border=0
-                )
+style.configure(
+    'Next.TButton',
+    foreground='white',
+    background='dark blue',
+    font=('Consolas', 16, 'bold')
+)
+
+style.configure(
+    'Sublabel.TLabel',
+    font=('Consolas', 12)
+)
 
 # Create the question label
 qs_label = ttk.Label(root, anchor="center", wraplength=500, padding=10)
-qs_label.pack(pady=10)
+qs_label.grid(row = 0, column = 0, columnspan = 5, pady = 0)
 
-#                                                                               Create the choice buttons
+noa_label = ttk.Label(root, anchor="center", wraplength=500, padding=10, style='Sublabel.TLabel')
+noa_label.grid(row = 1, column = 1, columnspan = 3, pady = 20)
+
+# Create the choice buttons
 choice_btns = []
 choice_btns_number = []
+
 for i in range(7):
     choice_btns_number.append(0)
     button = ttk.Button(root, command=lambda i=i: check_answer(i), style='Standard.TButton')
-    button.pack(pady=10)
+    button.grid(row=i + 2, column=1, columnspan=4, pady=5)
     choice_btns.append(button)
 
 # Create the next button
-next_btn = ttk.Button(root, text="Next", command=next_question, state="disabled")
-next_btn.pack(pady=20, side='bottom')
+next_btn = ttk.Button(root, text="Next", command=next_question, state="disabled", style='Next.TButton')
+next_btn.place(relx = 1, rely=1, x =-30, y = -65, anchor = 'ne')
 
 # Show the first question
 show_question()
